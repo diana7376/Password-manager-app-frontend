8b6d45e46616c57bbefe5caf1cedef6dba516dd3
Diana7376 <dianelup3105@gmail.com>
2024-09-06 12:22:54 +0300

    add modals on rowClick

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Table } from 'antd';
+import React, { useState } from 'react';
 import { data } from './data'
+import { Table, Modal } from 'antd';
+
 
 const columns = [
   {
-    title: 'App Name',
+    title: 'Name',
     dataIndex: 'app_name',
     key: 'app_name',
   },
   {
-    title: 'Username',
+    title: 'User_name',
     dataIndex: 'username',
     key: 'username',
   },
@@ -21,7 +22,54 @@ const columns = [
 ];
 
 const App = () => {
-  return <Table dataSource={data} columns={columns} rowKey="app_name" />;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedRow, setSelectedRow] = useState(null);
+
+  //store cliked row data and open modal
+  const handleRowClick = (record) => {
+    setSelectedRow(record);
+    setIsModalOpen(true);
+  };
+
+  //close modal and clear row data
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+    setSelectedRow(null);
+  };
+
+  return (
+      //main table
+      <>
+
+        <Table dataSource={data}
+               columns={columns}
+               rowKey="app_name"
+               onRow={(record) => {
+                 return {
+                   onClick: () => handleRowClick(record),
+                 };
+               }}
+
+        />
+        <br />
+
+        <Modal
+            title={selectedRow ? selectedRow.app_name : "Details"}
+            open={isModalOpen}
+            onOk={handleModalClose}
+            onCancel={handleModalClose}
+            okText="Save"
+        >
+          {selectedRow && (
+              <div>
+                <p><strong>Name:</strong> {selectedRow.app_name}</p>
+                <p><strong>User_name:</strong> {selectedRow.username}</p>
+                <p><strong>Password:</strong> {selectedRow.password}</p>
+              </div>
+          )}
+        </Modal>
+      </>
+  );
 };
 
 export default App;
