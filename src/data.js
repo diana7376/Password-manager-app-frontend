07768b06065c35fa2119390894b4d2d70b5d4513
Diana7376 <dianelup3105@gmail.com>
2024-09-10 11:44:59 +0300

    add CRUD operations + dataFetching caught error

--- src/data.js
+++
@@ -1,5 +0,0 @@
-export const data = [
-    {app_name: 'Facebook', username: 'mark', password: 'password'},
-    {app_name: 'Instagram', username: 'mark', password: 'password'},
-    {app_name: 'Twitter', username: 'mark', password: 'password'}
-];
