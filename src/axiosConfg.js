85b2f5e82b61176d31f11653d85ada7db364349c
Diana7376 <dianelup3105@gmail.com>
2024-09-17 16:42:38 +0300

    restructure of how routs work + added abouUs page

--- src/axiosConfg.js
+++ src/axiosConfg.js
@@ -4,9 +4,9 @@ const axiosInstance = axios.create({
     baseURL: 'http://127.0.0.1:8000/api/', // Update with your backend URL
 });
 
+export const token = localStorage.getItem('token');
 // Interceptor to add token to headers
 axiosInstance.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
 if (token) {
     config.headers['Authorization'] = `Bearer ${token}`;
 }
--- src/test.js
+++ src/test.js
@@ -1,24 +1,19 @@
 import React, { useEffect, useState, useRef } from 'react';
+
 import { Breadcrumb, Layout, Menu, theme, Input, Button, Modal, Space } from 'antd';
-import {
-    DesktopOutlined,
-    PieChartOutlined,
-    UserOutlined,
-    AudioOutlined,
-    LogoutOutlined,
-    LoginOutlined
-} from '@ant-design/icons';
+import { DesktopOutlined, PieChartOutlined, UserOutlined, AudioOutlined } from '@ant-design/icons';
+import {Route, Routes, useNavigate, useLocation, Navigate} from 'react-router-dom';
+
 import axios from './axiosConfg';
 import fuzzysort from 'fuzzysort';
 import MainPage from './main_page';
 import SaveNewPassword from './save_new_password';
-import { dataFetching, config, fetchAllPasswordItems, fetchUnlistedPasswordItems } from './crud_operation';
+import { dataFetching, fetchAllPasswordItems, fetchUnlistedPasswordItems } from './crud_operation';
 import './styles.css';
-import {Navigate, Route, Routes, useNavigate} from 'react-router-dom';
-import Login from './authorisation/login'; // Import Login component
-import Register from './authorisation/register'; // Import Register component
-import PrivateRoute from './authorisation/PrivateRoute';
-import AboutUs from "./aboutUs"; // Ensure this is the correct path
+import Login from './auth/auth';
+import Register from './auth/register';
+import PrivateRoute from './auth/PrivateRoute';
+import AboutUs from './AboutUs';
 
 
 const { Search } = Input;
@@ -33,7 +28,6 @@ const suffix = (
 
 const onSearch = (value, _e, info) => console.log(info?.source, value);
 
-// Function to get menu items for the sidebar
 function getItem(label, key, icon, children) {
     return {
         key,
@@ -46,35 +40,33 @@ function getItem(label, key, icon, children) {
 const App = () => {
     const [collapsed, setCollapsed] = useState(false);
     const [passwordItems, setPasswordItems] = useState([]);
-    const [groupItems, setGroupItems] = useState([]); // Initialize as an array
+    const [groupItems, setGroupItems] = useState([]);
     const [selectedGroupId, setSelectedGroupId] = useState(-1);
     const [userId, setUserId] = useState(1);
     const [comment, setCommentId] = useState(null);
     const [url, setUrlId] = useState(null);
+
     const [selectedKey, setSelectedKey] = useState('2'); // State to track selected menu item
     const [openKeys, setOpenKeys] = useState([]); // Track open submenu keys
     const [filteredItems, setFilteredItems] = useState([]); // For storing search results
     const [isSearching, setIsSearching] = useState(false); // Flag to track whether search is active
+
     const searchRef = useRef(null);
-    const searchInputRef = useRef(null); // Create ref for the actual input element
-
-    const [loggedIn, setLoggedIn] = useState(false); // State to track if user is logged in
-    const [showAuthModal, setShowAuthModal] = useState(false); // State to control the authentication modal
-
+    const searchInputRef = useRef(null);
+    const [loggedIn, setLoggedIn] = useState(false);
+    const location = useLocation();
     const navigate = useNavigate();
-
-    // Focus on the input when the component mounts
-    useEffect(() => {
-        if (searchInputRef.current) {
-            searchInputRef.current.focus(); // Focus on the input element
-        }
-    }, []);
 
     const {
         token: { colorBgContainer },
     } = theme.useToken();
 
-    // Check if the user is logged in
+    useEffect(() => {
+        if (searchInputRef.current) {
+            searchInputRef.current.focus();
+        }
+    }, []);
+
     useEffect(() => {
         const token = localStorage.getItem('token');
         setLoggedIn(!!token);
@@ -82,15 +74,10 @@ const App = () => {
 
     const fetchData = () => {
         if (selectedGroupId === -1) {
-            // Fetch data for all groups
             fetchAllPasswordItems(setPasswordItems);
-        }
-        else if (selectedGroupId) {
-            // Fetch data for a specific group
+        } else if (selectedGroupId) {
             dataFetching(selectedGroupId, setPasswordItems);
-        }
-        else {
-            // Fetch unlisted data
+        } else {
             fetchUnlistedPasswordItems(setPasswordItems);
         }
     };
@@ -99,26 +86,18 @@ const App = () => {
         fetchData();
     }, [selectedGroupId]);
 
-    // Fetch all groups
     useEffect(() => {
-        axios
-            .get('http://127.0.0.1:8000/api/groups/', { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } })
+        axios.get('http://127.0.0.1:8000/api/groups/', { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } })
             .then((response) => {
                 if (Array.isArray(response.data)) {
-
-                    // Create the default "All" and "Unlisted" groups
                     const allGroup = getItem('All', 'group-0');
                     const unlistedGroup = getItem('Unlisted', 'group-X');
-
-                    // Map fetched groups and prepend "All" and "Unlisted" groups
                     const fetchedGroups = [
-                        allGroup, // Insert "All" group first
+                        allGroup,
                         unlistedGroup,
                         ...response.data.map((group) => getItem(group.groupName, `group-${group.groupId}`))
                     ];
-
-                    setGroupItems(fetchedGroups); // Set the complete group list
-
+                    setGroupItems(fetchedGroups);
                 } else {
                     console.error('API response is not an array', response.data);
                     setGroupItems([]);
@@ -130,43 +109,26 @@ const App = () => {
             });
     }, []);
 
-    // Set up click event listener to detect clicks outside search bar
-    useEffect(() => {
-        const handleClickOutside = (event) => {
-            if (searchRef.current && !searchRef.current.contains(event.target)) {
-                setIsSearching(false); // Clear search state
-                setFilteredItems([]); // Clear search results
-            }
-        };
-
-        document.addEventListener('mousedown', handleClickOutside);
-        return () => document.removeEventListener('mousedown', handleClickOutside);
-    }, []);
-
     const onSearch = (value) => {
         setIsSearching(true);
-
         if (value.trim() === '') {
             setIsSearching(false);
-            setFilteredItems([]); // Clear search results if the query is empty
+            setFilteredItems([]);
             return;
         }
 
-        // Perform separate searches for each field
         const resultsItemName = fuzzysort.go(value, passwordItems, { key: 'itemName', threshold: -10000 });
         const resultsUserName = fuzzysort.go(value, passwordItems, { key: 'userName', threshold: -10000 });
         const resultsComment = fuzzysort.go(value, passwordItems, { key: 'comment', threshold: -10000 });
         const resultsUrl = fuzzysort.go(value, passwordItems, { key: 'url', threshold: -10000 });
 
-        // Combine the results, ensuring itemName results are prioritized
         const combinedResults = [
-            ...resultsItemName,            // Prioritize itemName results
-            ...resultsUserName,            // Followed by userName results
-            ...resultsComment,             // Then comment results
-            ...resultsUrl                  // Finally URL results
+            ...resultsItemName,
+            ...resultsUserName,
+            ...resultsComment,
+            ...resultsUrl,
         ];
 
-        // Create a Map to deduplicate entries (since items might appear in multiple fields)
         const uniqueResults = new Map();
         combinedResults.forEach(result => {
             if (!uniqueResults.has(result.obj.id)) {
@@ -174,14 +136,14 @@ const App = () => {
             }
         });
 
-        // Convert the Map back to an array of unique items
         const filtered = Array.from(uniqueResults.values()).map(result => ({
             ...result.obj,
-            similarityScore: result.score // Optionally include similarity score
+            similarityScore: result.score
         }));
 
-        setFilteredItems(filtered); // Update the filteredItems with the search results
+        setFilteredItems(filtered);
     };
+
 
     const handleMenuClick = (key) => {
         setSelectedKey(key); // Set the selected key when a menu item is clicked
@@ -245,36 +207,21 @@ const App = () => {
                         console.error('Error fetching group details:', error);
                     });
             }
+        }
+
+        // Define the onMenuSelect function to handle menu selection logic
+        else if (key === '1') {
+            navigate('/about');
+        } else if (key === 'login') {
+            navigate('/login');
+        } else if (key === 'register') {
+            navigate('/register'); // Navigate to the registration page
+
         } else if (key === 'logout') {
-            handleLogout(); // Call logout function when logout menu item is clicked
+            handleLogout();
         }
     };
 
-    const fetchDataForAllGroups = () => {
-        axios
-            .get('http://127.0.0.1:8000/api/password-items/', config) // Adjust API endpoint if necessary
-            .then((response) => {
-                setPasswordItems(response.data); // Assuming response contains all password items
-            })
-            .catch((error) => {
-                console.error('Error fetching all password items:', error);
-            });
-    };
-
-    const fetchDataForUnlistedGroups = () => {
-        axios
-            .get('http://127.0.0.1:8000/api/password-items/unlisted/', config) // Adjust API endpoint if necessary
-            .then((response) => {
-                setPasswordItems(response.data); // Assuming response contains unlisted password items
-            })
-            .catch((error) => {
-                console.error('Error fetching unlisted password items:', error);
-            });
-    };
-
-    const onOpenChange = (keys) => {
-        setOpenKeys(keys);
-    };
 
 
     const groupMenuItems = [
@@ -289,7 +236,6 @@ const App = () => {
                 label: 'Passwords',
                 key: '2',
                 icon: <PieChartOutlined />,
-                onClick: () => navigate('/passwords'),
             },
             {
                 label: 'Groups',
@@ -299,12 +245,13 @@ const App = () => {
             }
         ] : []),
         {
-            label: loggedIn ? 'Logout' : 'Login',
-            key: loggedIn ? 'logout' : 'login',
-            icon:loggedIn ? <LogoutOutlined /> : <LoginOutlined />,
-            onClick: () => loggedIn ? handleLogout() : navigate('/login')  // Navigate to /login when not logged in
+            label: loggedIn ? 'Logout' : 'Register',
+            key: loggedIn ? 'logout' : 'register',
+            icon: <UserOutlined />,
+            onClick: () => loggedIn ? handleLogout() : navigate('/register')  // Navigate to /register when not logged in
         }
     ];
+
 
     const [breadcrumbItems, setBreadcrumbItems] = useState([
         { title: 'Group' },
@@ -352,32 +299,32 @@ const App = () => {
                     mode="inline"
                     selectedKeys={[selectedKey]}
                     openKeys={openKeys}
-                    onClick={onMenuSelect}
-                    onOpenChange={onOpenChange}
+                    onClick={onMenuSelect} // Use the defined onMenuSelect function here
+                    onOpenChange={setOpenKeys}
                     items={groupMenuItems}
                 />
             </Sider>
 
             <Layout>
-                <div ref={searchRef}>
-                    <Search
-                        placeholder="input search text"
-                        onSearch={onSearch}
-                        style={{
-                            margin: '0 auto',
-                            padding: '50px 10px',
-                            // width: 1050,
-                            color: '#0a0a0a',
-                        }}
-                        ref={(input) => {
-                            // Attach the ref to the input DOM element inside the Search component
-                            if (input) {
-                                searchInputRef.current = input.input;
-                            }
-                        }}
-                    />
-                </div>
-                {/* <Header style={{ padding: 0, background: colorBgContainer }} />*/}
+                {/* Conditionally render the search bar based on the route */}
+                {location.pathname !== '/register' && location.pathname !== '/login' && (
+                    <div ref={searchRef}>
+                        <Search
+                            placeholder="input search text"
+                            onSearch={onSearch}
+                            style={{
+                                margin: '0 auto',
+                                padding: '50px 10px',
+                                color: '#0a0a0a',
+                            }}
+                            ref={(input) => {
+                                if (input) {
+                                    searchInputRef.current = input.input;
+                                }
+                            }}
+                        />
+                    </div>
+                )}
 
                 <Content style={{ margin: '0 16px' }}>
                     <Routes>
@@ -385,63 +332,20 @@ const App = () => {
                         <Route path="/register" element={<Register />} />
                         <Route path="/about" element={<AboutUs />} />
 
+                        {/* Default route*/}
                         <Route path="/" element={<Navigate to="/about" />} />
 
-                        <Route path="/passwords" element={
-                                <PrivateRoute>
-                                    <Breadcrumb style={{ margin: '16px 0' }} items={breadcrumbItems} />
-                                    <MainPage
-                                        passwordItems={isSearching ? filteredItems : passwordItems}
-                                        groupId={selectedGroupId}
-                                        userId={userId}
-                                    />
-                                </PrivateRoute>
-                            }
-                            exact
-                        />
-                    </Routes>
+                        <Route path="/main" element={
+                            <PrivateRoute>
+                                <MainPage
+                                    passwordItems = {isSearching ? filteredItems : passwordItems} groupId={selectedGroupId} userId={userId} />
 
-                    {/* Plus Button at the bottom-right corner under the table */}
-                    <div
-                        style={{
-                            position: 'fixed',
-                            bottom: 24,
-                            right: 24,
-                            zIndex: 1000,
-                        }}
-                    >
-                        <SaveNewPassword
-                            groupId={selectedGroupId}
-                            userId={userId}
-                            comment={comment}
-                            url={url}
-                            onPasswordAdd={onPasswordAdd}
-                        />
-                    </div>
+                            </PrivateRoute>
+                        } />
+                    </Routes>
                 </Content>
                 <Footer style={{ textAlign: 'center' }}>© 2024 LockR</Footer>
             </Layout>
-
-            <Modal
-                title="Authentication"
-                visible={showAuthModal}
-                onCancel={() => setShowAuthModal(false)}
-                footer={null}
-            >
-                {loggedIn ? (
-                    <p>You are logged in.</p>
-                ) : (
-                    <div>
-                        <p>Please log in to continue.</p>
-                        <Button type="primary" onClick={handleLogin} style={{ marginRight: 8 }}>
-                            Login
-                        </Button>
-                        <Button type="default" onClick={handleRegister}>
-                            Create Account
-                        </Button>
-                    </div>
-                )}
-            </Modal>
         </Layout>
     );
 };
